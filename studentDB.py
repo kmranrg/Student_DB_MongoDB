2d32ce6512d3ef3c5f881576c55b1780a1cc6b8c
Kumar Anurag <kan@Kumars-MacBook-Air.local>
2023-10-31 00:08:51 +0530

    all set

--- studentDB.py
+++ studentDB.py
@@ -16,9 +16,15 @@ class StudentDB():
             # reading the csv
             self.df = pd.read_csv(self.csv_path)
 
-            # Convert date strings to Python datetime objects
+            # converting date strings to Python datetime objects
             self.df["Joined_On"] = pd.to_datetime(self.df["Joined_On"])
             self.df["S_DOB"] = pd.to_datetime(self.df["S_DOB"])
+
+            # converting phone numbers to string format and also adding + in front
+            def add_plus(phone_no):
+                return "+" + phone_no
+            self.df["Personal_Phone"] = self.df["Personal_Phone"].astype(str).apply(add_plus)
+            self.df["Parent_Phone"] = self.df["Parent_Phone"].astype(str).apply(add_plus)
 
             # creating mongoDB client
             self.client = MongoClient(self.mongodb_uri)
@@ -128,7 +134,7 @@ class StudentDB():
             # mongodb query to get student data
             result = self.collection.find(
                 {
-                    "Student_Rating":{"$lt":7}
+                    "Student_Rating":{"$lt":4}
                 },
                 {
                     "Name":1,
@@ -194,6 +200,42 @@ class StudentDB():
             # closing the mongoDB connection
             self.client.close()
 
+    def query_database(self,user_query):
+        try:
+            # creating mongoDB client
+            self.client = MongoClient(self.mongodb_uri)
+
+            # accessing the specified database and connection
+            self.db = self.client[self.database_name]
+            self.collection = self.db[self.collection_name]
+
+            # mongodb query to get student data
+            result = self.collection.find(
+                user_query,
+                {
+                    "Name":1,
+                    "Email_ID":1,
+                    "Student_Rating":1,
+                    "S_DOB":1,
+                    "Country":1,
+                    "Personal_Phone":1,
+                    "_id":0
+                }
+            )
+
+            students = []
+            for student in result:
+                student["S_DOB"] = parser.parse(str(student["S_DOB"])).strftime("%B %d, %Y")
+                students.append(student)
+            return students
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+        finally:
+            # closing the mongoDB connection
+            self.client.close()
+
     def drop_database(self):
         # Connect to MongoDB
         client = MongoClient("mongodb://localhost:27017")
@@ -206,10 +248,11 @@ class StudentDB():
         finally:
             return "Database has been dropped."
 
-students = StudentDB()
+# students = StudentDB()
 # print(students.load_data_from_csv())
 # print(students.get_all_students())
 # print(students.rating_more_than_7())
 # print(students.rating_less_than_7())
 # print(students.born_after_june_01_1999())
 # print(students.drop_database())
+# print(students.query_database({"Country":"Hong Kong"}))
